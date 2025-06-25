33ddff77139280a5ef67c972a6560fc1051e889c
Harshitaaa245 <harshitamemory@gmail.com>
2025-06-25 20:23:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
+console.log(ctx);
+ctx.fillStyle = 'white';
+ctx.arc(100, 100, 50, 0, Math.PI*2 );
+ctx.fill();
