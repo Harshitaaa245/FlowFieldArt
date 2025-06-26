8fb149a0087c886cd9fafafad88b6228c754376a
Harshitaaa245 <harshitamemory@gmail.com>
2025-06-26 14:25:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -15,16 +15,32 @@ class Particle
       this.effect = effect;
       this.x = Math.floor(Math.random()*this.effect.width);
       this.y = Math.floor(Math.random()*this.effect.height);
+      this.speedX = Math.random()*5 - 2.5;
+      this.speedY = Math.random()*5 - 2.5;
+      this.history = [{x: this.x, y:this.y}];
+      this.maxLength = Math.floor(Math.random()*100 + 10);
+      
     }
     draw(context)
     {
       context.fillRect(this.x,this.y,10,10);
+      context.beginPath();
+      context.moveTo(this.history[0].x, this.history[0].y);
+      for(let i=0; i<this.history.length; i++)
+        {
+          context.lineTo(this.history[i].x, this.history[i].y);
+        }
+      conctext.stroke();
     }
     update()
     {
-      this.x += this.effect.speedX;
-      this.y += this.effect.speedY;
-      
+      this.x += this.speedX + Math.random()*15 - 7.5;
+      this.y += this.speedY + Math.random()*15 - 7.5;
+      this.history.push({x: this.x, y:this.y});
+      if(this.history.length>this.maxLength)
+      {
+        this.history.shift();
+      }
     }
   }
 
@@ -37,9 +53,6 @@ class Effect
       this.particles = [];
       this.numberOfParticles = 50;
       this.init();
-      this.speedX = 1;
-      this.speedY = 1;
-      
     }
 
     init()
