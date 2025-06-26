4fe67ba26fd5e502dda2223190bfcae147b256ae
Harshitaaa245 <harshitamemory@gmail.com>
2025-06-26 16:04:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,29 +11,28 @@ ctx.lineWidth = 1;
 class Particle {
   constructor(effect, x, y) {
     this.effect = effect;
-    this.x = x;
-    this.y = y;
     this.originX = x;
     this.originY = y;
-    this.speedX = Math.random() * 5 - 2.5;
-    this.speedY = Math.random() * 5 - 2.5;
+    this.x = x;
+    this.y = y;
+    this.speedX = Math.random() * 2 - 1;
+    this.speedY = Math.random() * 2 - 1;
     this.history = [{ x: this.x, y: this.y }];
-    this.maxLength = Math.floor(Math.random() * 100 + 10);
+    this.maxLength = Math.floor(Math.random() * 50 + 20);
   }
 
   draw(context) {
-    context.fillRect(this.x, this.y, 2, 2); // Smaller dots
     context.beginPath();
     context.moveTo(this.history[0].x, this.history[0].y);
-    for (let i = 0; i < this.history.length; i++) {
+    for (let i = 1; i < this.history.length; i++) {
       context.lineTo(this.history[i].x, this.history[i].y);
     }
     context.stroke();
   }
 
   update() {
-    this.x = this.originX + Math.sin(Date.now() * 0.001 + this.originX) * 2;
-    this.y = this.originY + Math.cos(Date.now() * 0.001 + this.originY) * 2;
+    this.x += this.speedX;
+    this.y += this.speedY;
     this.history.push({ x: this.x, y: this.y });
     if (this.history.length > this.maxLength) {
       this.history.shift();
@@ -54,28 +53,28 @@ class Effect {
       for (let x = 0; x < imageData.width; x += 4) {
         const index = (y * imageData.width + x) * 4;
         const alpha = imageData.data[index + 3];
-        if (alpha > 128) {
-          this.particles.push(new Particle(this, x, y));
+        if (alpha > 100) {
+          const mappedX = x * (canvas.width / imageData.width);
+          const mappedY = y * (canvas.height / imageData.height);
+          this.particles.push(new Particle(this, mappedX, mappedY));
         }
       }
     }
   }
 
   render(context) {
-    this.particles.forEach((particle) => {
-      particle.update();
-      particle.draw(context);
+    this.particles.forEach(p => {
+      p.update();
+      p.draw(context);
     });
   }
 }
 
 const effect = new Effect(canvas.width, canvas.height);
 
-// Load and process image
 const image = new Image();
-image.src = 'img/art1.jpg'; // Make sure this path is correct
+image.src = 'img/art1.jpg'; // replace with your actual image path
 image.onload = () => {
-  // Draw image to hidden canvas
   ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
   const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
   ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -88,4 +87,5 @@ function animate() {
   effect.render(ctx);
   requestAnimationFrame(animate);
 }
+
 
